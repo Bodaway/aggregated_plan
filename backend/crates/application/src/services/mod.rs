1a4c5371a02082547517338f4e8d81722f03d100
MBonenfant <bodaway@outlook.com>
2026-06-08 17:48:57 +0200

    feat(outlook): OutlookTokenProvider trait

--- backend/crates/application/src/services/mod.rs
+++ backend/crates/application/src/services/mod.rs
@@ -1,7 +1,9 @@
 pub mod jira_client;
 pub mod outlook_client;
 pub mod excel_client;
+pub mod outlook_token_provider;
 
 pub use jira_client::*;
 pub use outlook_client::*;
 pub use excel_client::*;
+pub use outlook_token_provider::*;
